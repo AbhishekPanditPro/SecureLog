4bce790f64bd52f5a3730946d35602f0ca195bec
Abhishek Pandit <apandit3@kent.edu>
2024-08-26 11:10:46 -0400

    new files logappend and logread added:

---
+++ logappend.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// to run the c++ command use the g++ or clang++ 
+// example: g++ logappend.cpp -o logappend
+//                 ^               ^
+//               c++ filename     executable filename
+
+
+int main(){
+    // std::cout prints the string after the "<<" insertion operator. 
+    // Note: you can do multiple insertion and at the end std::endl to end the line.
+    std::cout << "Hello logappend!" << std::endl;
+}
