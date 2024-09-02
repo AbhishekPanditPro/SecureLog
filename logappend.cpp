3db9a9da26b3f250e0f68cd5a5fd41c4350eb6df
Abhishek Pandit <apandit3@kent.edu>
2024-09-02 10:33:21 -0400

    Some code for the command line added

--- logappend.cpp
+++ logappend.cpp
@@ -1,3 +1,5 @@
+// this file will append new information to the file
+
 #include <iostream>
 
 // to run the c++ command use the g++ or clang++ 
@@ -6,8 +8,14 @@
 //               c++ filename     executable filename
 
 
-int main(){
+int main(int argc, char* argv[]){
     // std::cout prints the string after the "<<" insertion operator. 
     // Note: you can do multiple insertion and at the end std::endl to end the line.
+   
+    // this argc and argv is used for inputting codes in command line while executing the object file
+   for( int i = 1; i < argc; i++){
+        std::cout << argv[i] << std::endl;
+    }
+
     std::cout << "Hello logappend!" << std::endl;
 }
--- logread.cpp
+++ logread.cpp
@@ -1,6 +1,7 @@
+// this file will read from the file and display the state of the art gallery according to a query given over the log
 #include <iostream>
 
 int main(){
     std::cout << "Hello logread!" << std::endl;
-    
+
 }
